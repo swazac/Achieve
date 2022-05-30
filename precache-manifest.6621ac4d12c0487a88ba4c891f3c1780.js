4ad715e2b258f23769ab12b3f8cd0e58980f1f75
Swarup Zachariah <Swarup.zac@gmail.com>
2022-05-30 14:25:43 -0400

    Updates

---
+++ precache-manifest.6621ac4d12c0487a88ba4c891f3c1780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029f71e3d45231250fa8dcec8e60a010",
+    "url": "/Achieve/index.html"
+  },
+  {
+    "revision": "9c100a5ed37171172e47",
+    "url": "/Achieve/static/css/main.89d5b3cb.chunk.css"
+  },
+  {
+    "revision": "e2322f2400c70bd58ff4",
+    "url": "/Achieve/static/js/2.5f8b801f.chunk.js"
+  },
+  {
+    "revision": "9c100a5ed37171172e47",
+    "url": "/Achieve/static/js/main.12d302f3.chunk.js"
+  },
+  {
+    "revision": "1bde9078fef9e679fed8",
+    "url": "/Achieve/static/js/runtime-main.fd73467c.js"
+  }
+]);
